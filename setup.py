9e0378074ee7919780ec9ff97aa4636e5da0a99e
Wagner Augusto Andreoli <wagnerandreoli@gmail.com>
2015-03-17 01:33:25 +0100

    Remove all support for Python 2.7 and PyPy

--- setup.py
+++ setup.py
@@ -45,13 +45,7 @@ setup(
         'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Utilities',
     ],
     keywords=[
