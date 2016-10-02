a2506648105e8e08764ddcecd03d803c676f67fa
Wagner Augusto Andreoli <andreoliwa@gmail.com>
2016-10-02 19:27:50 +0200

    Use a decorator for the command handlers

--- dontforget/ui/telegram_bot.py
+++ dontforget/ui/telegram_bot.py
@@ -1,7 +1,7 @@
 """Telegram bot module."""
-# pylint: disable=no-self-use
 import logging
 from datetime import datetime
+from functools import wraps
 
 import telegram
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
@@ -10,65 +10,93 @@ from dontforget.models import Alarm, AlarmState
 from dontforget.settings import UI_TELEGRAM_BOT_TOKEN
 
 
+def handler_callback(method):
+    """Decorator for command methods in the :py:class:`TelegramBot` class."""
+    @wraps(method)
+    def method_wrapper(self):
+        """Wrapper around methods in the :py:class:`TelegramBot` class.
+
+        :param self: A :py:class:`TelegramBot` instance.
+        :return: Pointer to the function called by the framework.
+        """
+        def function_called_by_bot_framework(bot, update, args=None):   # pylint: disable=invalid-name
+            """Function called by the Telegram bot framework.
+
+            Don't change the names of the arguments; the framework expects them.
+
+            :param bot: Telegram bot object sent by the framework.
+            :param update: Message update
+            :param args: Extra arguments when `pass_args=True` is used.
+            :return: Result of calling `method(self)`.
+            """
+            self.bot = bot
+            self.update = update
+            self.args = args
+            return method(self)
+        return function_called_by_bot_framework
+    return method_wrapper
+
+
 class TelegramBot:
     """Telegram bot to answer commands."""
 
     def __init__(self, app):
+        """Init the instance of the bot.
+
+        Some properties will be filled by the decorator :py:meth:`handler_callback()`.
+
+        :param app: Flask app.
+        """
         self.app = app
+        self.bot = None
+        self.update = None
+        self.args = None
 
+    def send_message(self, text, reply_markup=None):
+        """Send a message to the Telegram chat.
+
+        :param text: Text to send.
+        :param reply_markup: Optional buttons/markup to the chat.
+        """
+        self.bot.send_message(chat_id=self.update.message.chat_id, text=text, reply_markup=reply_markup)
+
+    @handler_callback
     def start(self):
-        """Start the bot."""
-        def callback(bot, update):
-            """Function called by the bot when there is an update."""
-            bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me?")
-        return callback
+        """Start the bot with a friendly message."""
+        self.send_message("I'm a bot to help you with your chores.")
 
+    @handler_callback
     def echo(self):
         """Echo a message."""
-        def callback(bot, update):
-            """Function called by the bot when there is an update."""
-            bot.sendMessage(chat_id=update.message.chat_id, text='Current time: {0}, text received: {1}'.format(
-                datetime.now().isoformat(), update.message.text))
-        return callback
+        self.send_message('Current time: {0}, text received: {1}'.format(
+            datetime.now().isoformat(), self.update.message.text))
 
+    @handler_callback
     def caps(self):
         """Change text to uppercase."""
-        def callback(bot, update, args):
-            """Function called by the bot when there is an update."""
-            text_caps = ' '.join(args).upper()
-            bot.sendMessage(chat_id=update.message.chat_id, text=text_caps)
-        return callback
+        self.send_message(' '.join(self.args).upper())
 
+    @handler_callback
     def chore(self):
         """Show options for a chore."""
-        def callback(bot, update):
-            """Function called by the bot when there is an update."""
-            custom_keyboard = [['Skip', 'Snooze'], ['Complete', 'Abort']]
-            reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
-            bot.sendMessage(chat_id=update.message.chat_id, text='What do you want to do with this chore?',
-                            reply_markup=reply_markup)
-        return callback
+        custom_keyboard = [['Skip', 'Snooze'], ['Complete', 'Abort']]
+        self.send_message('What do you want to do with this chore?', telegram.ReplyKeyboardMarkup(custom_keyboard))
 
+    @handler_callback
     def overdue(self):
         """Overdue tasks."""
-        def callback(bot, update):
-            """Function called by the bot when there is an update."""
-            right_now = datetime.now()
-            with self.app.app_context():
-                # pylint: disable=no-member
-                query = Alarm.query.filter(Alarm.current_state == AlarmState.UNSEEN,
-                                           Alarm.next_at <= right_now).order_by(Alarm.id)
-                all_alarms = [str(unseen_alarm) for unseen_alarm in query.all()]
-                bot.sendMessage(chat_id=update.message.chat_id, text='Those are your overdue chores:\n{}'.format(
-                    '\n'.join(all_alarms)))
-        return callback
+        right_now = datetime.now()
+        with self.app.app_context():
+            # pylint: disable=no-member
+            query = Alarm.query.filter(Alarm.current_state == AlarmState.UNSEEN,
+                                       Alarm.next_at <= right_now).order_by(Alarm.id)
+            all_alarms = [str(unseen_alarm) for unseen_alarm in query.all()]
+            self.send_message('Those are your overdue chores:\n{}'.format('\n'.join(all_alarms)))
 
+    @handler_callback
     def unknown(self):
         """Unknown command."""
-        def callback(bot, update):
-            """Function called by the bot when there is an update."""
-            bot.sendMessage(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
-        return callback
+        self.send_message("Sorry, I didn't understand that command.")
 
     def run_loop(self):
         """Run the main loop for the Telegram bot."""
