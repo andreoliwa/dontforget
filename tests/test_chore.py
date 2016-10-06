f2d06d9c31458e7a214287328acf02bd45969f26
Wagner Augusto Andreoli <andreoliwa@gmail.com>
2016-10-07 00:17:28 +0200

    Try to fix stupid isort

--- tests/test_chore.py
+++ tests/test_chore.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# pylint: disable=invalid-name,no-member
 """Test chores."""
+# pylint: disable=invalid-name,no-member
 from datetime import datetime, timedelta
 
 from tests.factories import NEXT_WEEK, TODAY, YESTERDAY, ChoreFactory
